ef08592dc07ddc291ae10fa93118d2d062b9610e
Yuluka Gigante <Yulukeitor@gmail.com>
2021-02-26 22:35:23 -0500

    Second version with all the requirements, except the two decimals (it shows the decimals that it want)

--- ui/Main.java
+++ ui/Main.java
@@ -1,6 +1,6 @@
 package ui;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,13 +8,20 @@ public class Main {
 	public static final Scanner scStr = new Scanner(System.in);
 	
 	public static double[] ages;
+	public static double average;
 	
 	public static void main(String[] args) {
 		int testCases = scNums.nextInt();
 		
+		String agesAux;
+		String[] agesStr;
+		
+		ArrayList<double[]> orderedLists = new ArrayList<>();
+		ArrayList<Double> listsAverages = new ArrayList<>();
+		
 		for (int i = 0; i < testCases ;i++) {
-			String agesAux = scStr.nextLine();
-			String[] agesStr=agesAux.split(" ");
+			agesAux = scStr.nextLine();
+			agesStr=agesAux.split(" ");
 			ages=new double[agesStr.length];
 			
 			for(int j=0; j<agesStr.length; j++) {
@@ -22,18 +29,28 @@ public class Main {
 				
 			}
 			
-			bubbleSort();
-			System.out.println(Arrays.toString(ages));
+			orderedLists.add(bubbleSort());
+			listsAverages.add(average);
+		}
+		
+		for(int i=0; i<orderedLists.size(); i++) {
+			String nums="";
+			
+			for (int j = 0; j < orderedLists.get(i).length; j++) {
+				nums+=" " + orderedLists.get(i)[i];
+			}
+			
+			System.out.println(listsAverages.get(i) + "-" + nums);
+			
 		}
 	}
 	
-	public static void bubbleSort(){
-		boolean changed=true;
+	public static double[] bubbleSort(){
+		int iterations=0;
+		int control=0;
 		
-		for(int i=1; i<ages.length && changed; i++) {
-			
-			changed=false;
-			
+		for(int i=1; i<ages.length; i++) {
+		
 			for(int j=0; j<ages.length-i;j++) {
 				if(ages[j]>ages[j+1]) {
 					double temp=ages[j];
@@ -41,9 +58,15 @@ public class Main {
 					ages[j]=ages[j+1];
 					ages[j+1]=temp;
 					
-					changed=true;
+					control++;
 				}
 			}
+			
+			iterations++;
 		}
+		
+		average=control*1.0/iterations;
+		
+		return ages;
 	}
 }
